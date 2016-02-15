cf0470dc0aa656bf0dbe02d8a1642262edd20773
José Cassio dos Santos Jr <jcassiojr@icloud.com>
2016-02-15 12:31:03 -0200

    deixei somente tratamento de primeiro pgto e os sms e telefones obtidos dos arquivos enviados pelo Rafael

--- R/analise_ts.R
+++ R/analise_ts.R
@@ -12,63 +12,63 @@ l_raw <- f_leRawCobra(valor_divida)
 # obtém dataframe
 df_acion <- l_raw[[1]]
 df_carteira <- l_raw[[2]]
-#df_pg <- l_raw[[3]]
+#df_pg <- l_raw[[3]] # usando pgtos de todas as carteiras abaixo
 
 # ACIONAMENTOS
 ##################
 # filtrando por Ativo
-df_acion.atv <-
-    df_acion %>%
-    filter(grepl("Ativo",TIPO.ACION))
+#df_acion.atv <-
+#    df_acion %>%
+#    filter(grepl("Ativo",TIPO.ACION))
 # eliminando NAs
-df_acion.atv <- na.omit(df_acion.atv)
+#df_acion.atv <- na.omit(df_acion.atv)
 
 # filtrando por Receptivo
-df_acion.rec <-
-    df_acion %>%
-    filter(grepl("Receptivo",TIPO.ACION))
+#df_acion.rec <-
+#    df_acion %>%
+#    filter(grepl("Receptivo",TIPO.ACION))
 # eliminando NAs
-df_acion.rec <- na.omit(df_acion.rec)
+#df_acion.rec <- na.omit(df_acion.rec)
 
 # filtrando por SMS
-df_acion.sms <-
-    df_acion %>%
-    filter(grepl("Envio de Sms",OCORRENCIA))
+#df_acion.sms <-
+#    df_acion %>%
+#    filter(grepl("Envio de Sms",OCORRENCIA))
 # eliminando NAs
-df_acion.sms <- na.omit(df_acion.sms)
+#df_acion.sms <- na.omit(df_acion.sms)
 
 # filtrando por Ativo, Receptivo
-df_acion.fone <-
-    df_acion %>%
-    filter(grepl("Ativo|Receptivo",TIPO.ACION))
+#df_acion.fone <-
+#    df_acion %>%
+#    filter(grepl("Ativo|Receptivo",TIPO.ACION))
 # eliminando NAs
-df_acion.fone <- na.omit(df_acion.fone)
+#df_acion.fone <- na.omit(df_acion.fone)
 
 # somando os Ativo, c, SMS
-df_acion.sms_fone <-
-    df_acion %>%
-    filter(grepl("Ativo|Receptivo",TIPO.ACION) |
-               grepl("Envio de Sms",OCORRENCIA))
+#df_acion.sms_fone <-
+#    df_acion %>%
+#    filter(grepl("Ativo|Receptivo",TIPO.ACION) |
+#               grepl("Envio de Sms",OCORRENCIA))
 # eliminando NAs
-df_acion.sms_fone <- na.omit(df_acion.sms_fone)
+#df_acion.sms_fone <- na.omit(df_acion.sms_fone)
 
 # ACIONAMENTOS DE SMS E FONE
 ##################
 
 #  obs: tem que converter para POSIXct para funcionar com dplyr!!!!
-df_acion.sms_fone$DIA.ACION <- as.POSIXct(trunc.POSIXt(df_acion.sms_fone$DATA.ACION, units = "days"))
+#df_acion.sms_fone$DIA.ACION <- as.POSIXct(trunc.POSIXt(df_acion.sms_fone$DATA.ACION, units = "days"))
 # agrupando por dia 
-df_acion_dia <-
-    df_acion.sms_fone %>%
-    group_by(DIA.ACION) %>%
-    summarise(acions.dia = n())
+#df_acion_dia <-
+#    df_acion.sms_fone %>%
+#    group_by(DIA.ACION) %>%
+#    summarise(acions.dia = n())
 # eliminando NAs
-df_acion_dia <- na.omit(df_acion_dia)
+#df_acion_dia <- na.omit(df_acion_dia)
 
 # ggplot 
-pl_ac <- ggplot(df_acion_dia, aes(DIA.ACION, acions.dia)) + geom_line() + geom_smooth() +
-    xlab("dia") + ylab("acionamentos") + ggtitle("Acionamentos (SMS e Fone)/Dia") +
-    xlim(c(min(df_acion_dia$DIA.ACION),max(df_acion_dia$DIA.ACION)))
+#pl_ac <- ggplot(df_acion_dia, aes(DIA.ACION, acions.dia)) + geom_line() + geom_smooth() +
+#    xlab("dia") + ylab("acionamentos") + ggtitle("Acionamentos (SMS e Fone)/Dia") +
+#    xlim(c(min(df_acion_dia$DIA.ACION),max(df_acion_dia$DIA.ACION)))
 
 
 # PGTO 
@@ -101,26 +101,20 @@ df_pg <-
 #df_pg <- df_pg %>% mutate (VlPag = as.numeric(VlPag)) # para acerto do merge com cliav
 
 # número de pgtos agrupando por dia 
-df_pg_dia <-
-    df_pg %>%
-    group_by(DTpgto) %>%
-    summarise(pgto.dia = n(),
-              vlpg.dia = sum(VlPag))
-
-# plot de nro pgtos dia
-pl_pg <- ggplot(df_pg_dia, aes(DTpgto, pgto.dia)) + geom_line() + geom_smooth() +
-    xlab("dia") + ylab("pagamentos") + ggtitle("Número de Pagamentos/Dia") + 
-    xlim(c(min(df_acion_dia$DIA.ACION),max(df_acion_dia$DIA.ACION)))
-
-# valor de pgtos agrupando por dia 
-#df_vlpg_dia <-
+#df_pg_dia <-
 #    df_pg %>%
 #    group_by(DTpgto) %>%
-#    summarise(vlpg.dia = sum(VlPag))
+#    summarise(pgto.dia = n(),
+#              vlpg.dia = sum(VlPag))
 
-pl_vlpg <- ggplot(df_vlpg_dia, aes(DTpgto, vlpg.dia)) + geom_line() + geom_smooth() +
-    xlab("dia") + ylab("pagamentos") + ggtitle("Valor de Pagamentos/Dia") + 
-    xlim(c(min(df_acion_dia$DIA.ACION),max(df_acion_dia$DIA.ACION)))
+# plot de nro pgtos dia
+#pl_pg <- ggplot(df_pg_dia, aes(DTpgto, pgto.dia)) + geom_line() + geom_smooth() +
+#    xlab("dia") + ylab("pagamentos") + ggtitle("Número de Pagamentos/Dia") + 
+#    xlim(c(min(df_acion_dia$DIA.ACION),max(df_acion_dia$DIA.ACION)))
+
+#pl_vlpg <- ggplot(df_vlpg_dia, aes(DTpgto, vlpg.dia)) + geom_line() + geom_smooth() +
+#    xlab("dia") + ylab("pagamentos") + ggtitle("Valor de Pagamentos/Dia") + 
+#    xlim(c(min(df_acion_dia$DIA.ACION),max(df_acion_dia$DIA.ACION)))
 
 # IMPORTANTE ABAIXO: considerando somente o primeiro pgto de cada contrato/dia
 # considerando somente o primiro pagamento de cada contrato
@@ -155,83 +149,82 @@ pl_prim_vlpg <- ggplot(df_pg.primpg_dia, aes(DTpgto,vlpg.dia)) + geom_line() + g
 # ACIONAMENTOS DE SMS
 ##################
 #  obs: tem que converter para POSIXct para funcionar com dplyr!!!!
-df_acion.sms$DIA.ACION <- as.POSIXct(trunc.POSIXt(df_acion.sms$DATA.ACION, units = "days"))
+#df_acion.sms$DIA.ACION <- as.POSIXct(trunc.POSIXt(df_acion.sms$DATA.ACION, units = "days"))
 # agrupando por dia 
-df_acion_sms_dia <-
-    df_acion.sms %>%
-    group_by(DIA.ACION) %>%
-    summarise(acions.dia = n())
+#df_acion_sms_dia <-
+#    df_acion.sms %>%
+#    group_by(DIA.ACION) %>%
+#    summarise(acions.dia = n())
 # POR DIA
 #ts_acion_dia <- ts(df_acion_dia$acions.dia, frequency=365, start=c(2014,365))
 #plot(ts_acion_dia)
 # ggplot 
-pl_ac.sms <- ggplot(df_acion_sms_dia, aes(DIA.ACION, acions.dia)) + geom_line() + geom_smooth() +
-    xlab("dia") + ylab("acionamentos") + ggtitle("Acionamentos (SMS)/Dia") +
-    xlim(c(min(df_acion_sms_dia$DIA.ACION),max(df_acion_sms_dia$DIA.ACION)))
+#pl_ac.sms <- ggplot(df_acion_sms_dia, aes(DIA.ACION, acions.dia)) + geom_line() + geom_smooth() +
+#    xlab("dia") + ylab("acionamentos") + ggtitle("Acionamentos (SMS)/Dia") +
+#    xlim(c(min(df_acion_sms_dia$DIA.ACION),max(df_acion_sms_dia$DIA.ACION)))
 
 # ACIONAMENTOS DE ATIVO
 ##################
 #  obs: tem que converter para POSIXct para funcionar com dplyr!!!!
-df_acion.atv$DIA.ACION <- as.POSIXct(trunc.POSIXt(df_acion.atv$DATA.ACION, units = "days"))
+#df_acion.atv$DIA.ACION <- as.POSIXct(trunc.POSIXt(df_acion.atv$DATA.ACION, units = "days"))
 # agrupando por dia 
-df_acion_atv_dia <-
-    df_acion.atv %>%
-    group_by(DIA.ACION) %>%
-    summarise(acions.dia = n())
+#df_acion_atv_dia <-
+#    df_acion.atv %>%
+#    group_by(DIA.ACION) %>%
+#    summarise(acions.dia = n())
 # POR DIA
 #ts_acion_dia <- ts(df_acion_dia$acions.dia, frequency=365, start=c(2014,365))
 #plot(ts_acion_dia)
 # ggplot 
-pl_ac.atv <- ggplot(df_acion_atv_dia, aes(DIA.ACION, acions.dia)) + geom_line() + geom_smooth() +
-    xlab("dia") + ylab("acionamentos") + ggtitle("Acionamentos (Ativo)/Dia") +
-    xlim(c(min(df_acion_atv_dia$DIA.ACION),max(df_acion_atv_dia$DIA.ACION)))
+#pl_ac.atv <- ggplot(df_acion_atv_dia, aes(DIA.ACION, acions.dia)) + geom_line() + geom_smooth() +
+#    xlab("dia") + ylab("acionamentos") + ggtitle("Acionamentos (Ativo)/Dia") +
+#    xlim(c(min(df_acion_atv_dia$DIA.ACION),max(df_acion_atv_dia$DIA.ACION)))
 
 # ACIONAMENTOS DE RECEPTIVO
 ##################
 #  obs: tem que converter para POSIXct para funcionar com dplyr!!!!
-df_acion.rec$DIA.ACION <- as.POSIXct(trunc.POSIXt(df_acion.rec$DATA.ACION, units = "days"))
+#df_acion.rec$DIA.ACION <- as.POSIXct(trunc.POSIXt(df_acion.rec$DATA.ACION, units = "days"))
 # agrupando por dia 
-df_acion_rec_dia <-
-    df_acion.rec %>%
-    group_by(DIA.ACION) %>%
-    summarise(acions.dia = n())
-df_acion_rec_dia <- na.omit(df_acion_rec_dia)
+#df_acion_rec_dia <-
+#    df_acion.rec %>%
+#    group_by(DIA.ACION) %>%
+#    summarise(acions.dia = n())
+#df_acion_rec_dia <- na.omit(df_acion_rec_dia)
 # POR DIA
 #ts_acion_dia <- ts(df_acion_dia$acions.dia, frequency=365, start=c(2014,365))
 #plot(ts_acion_dia)
 # ggplot 
-pl_ac.rec <- ggplot(df_acion_rec_dia, aes(DIA.ACION, acions.dia)) + geom_line() + geom_smooth() +
-    xlab("dia") + ylab("acionamentos") + ggtitle("Acionamentos (Receptivo)/Dia") +
-    xlim(c(min(df_acion_rec_dia$DIA.ACION),max(df_acion_rec_dia$DIA.ACION)))
+#pl_ac.rec <- ggplot(df_acion_rec_dia, aes(DIA.ACION, acions.dia)) + geom_line() + geom_smooth() +
+#    xlab("dia") + ylab("acionamentos") + ggtitle("Acionamentos (Receptivo)/Dia") +
+#    xlim(c(min(df_acion_rec_dia$DIA.ACION),max(df_acion_rec_dia$DIA.ACION)))
 #grid.arrange(pl_ac,pl_pg, nrow=2, ncol=1)
 # outra alternativa a grid
 # plot por acionamento
 
-pushViewport(viewport(layout = grid.layout(4, 1)))
-#print(pl_ac, vp = viewport(layout.pos.row = 1, layout.pos.col = 1))
-print(pl_pg, vp = viewport(layout.pos.row = 1, layout.pos.col = 1))
-print(pl_ac.sms, vp = viewport(layout.pos.row = 2, layout.pos.col = 1))
-print(pl_ac.atv, vp = viewport(layout.pos.row = 3, layout.pos.col = 1))
-print(pl_ac.rec, vp = viewport(layout.pos.row = 4, layout.pos.col = 1))
+#pushViewport(viewport(layout = grid.layout(4, 1)))
+#print(pl_pg, vp = viewport(layout.pos.row = 1, layout.pos.col = 1))
+#print(pl_ac.sms, vp = viewport(layout.pos.row = 2, layout.pos.col = 1))
+#print(pl_ac.atv, vp = viewport(layout.pos.row = 3, layout.pos.col = 1))
+#print(pl_ac.rec, vp = viewport(layout.pos.row = 4, layout.pos.col = 1))
 
 # Mesmos plots por contrato
 #-----------------------------------------
-df_pg.contr <-
-    df_pg %>%
-    arrange(CONTRATO,DTpgto) %>%
-    group_by(CONTRATO) %>%
-    mutate(VALOR.PAGO = sum(VlPag),
-           NPARCELAS.PAGAS = n()) %>% # aqui cuidado, pois algumas parcelas se repetiram (??)
-    distinct(CONTRATO) %>%
-    select(-VlPag)
+#df_pg.contr <-
+#    df_pg %>%
+#    arrange(CONTRATO,DTpgto) %>%
+#    group_by(CONTRATO) %>%
+#    mutate(VALOR.PAGO = sum(VlPag),
+#           NPARCELAS.PAGAS = n()) %>% # aqui cuidado, pois algumas parcelas se repetiram (??)
+#    distinct(CONTRATO) %>%
+#    select(-VlPag)
 # número de parcelas pagas por dia
-pl_pg.rparc <- ggplot(df_pg.contr, aes(DTpgto, NPARCELAS.PAGAS)) + geom_line() + geom_smooth() +
-    xlab("dia") + ylab("pagamentos") + ggtitle("Número de Parcelas/Dia") + 
-    xlim(c(min(df_acion_dia$DIA.ACION),max(df_acion_dia$DIA.ACION)))
+#pl_pg.rparc <- ggplot(df_pg.contr, aes(DTpgto, NPARCELAS.PAGAS)) + geom_line() + geom_smooth() +
+#    xlab("dia") + ylab("pagamentos") + ggtitle("Número de Parcelas/Dia") + 
+#    xlim(c(min(df_acion_dia$DIA.ACION),max(df_acion_dia$DIA.ACION)))
 # valor total pago por dia
-pl_pg.ttpg <- ggplot(df_pg.contr, aes(DTpgto, VALOR.PAGO)) + geom_line() + geom_smooth() +
-    xlab("dia") + ylab("pagamentos") + ggtitle("Valor Total Pago/Dia") + 
-    xlim(c(min(df_acion_dia$DIA.ACION),max(df_acion_dia$DIA.ACION)))
+#pl_pg.ttpg <- ggplot(df_pg.contr, aes(DTpgto, VALOR.PAGO)) + geom_line() + geom_smooth() +
+#    xlab("dia") + ylab("pagamentos") + ggtitle("Valor Total Pago/Dia") + 
+#    xlim(c(min(df_acion_dia$DIA.ACION),max(df_acion_dia$DIA.ACION)))
 
 # TESTANDO OS ARQUIVOS RECEBIDOS DE SMS E TELEFONIA DO RAFAEL
 # 1. obter a quantidade de SMS enviados, agrupados por: com ou sem confirmação
@@ -909,7 +902,7 @@ df_sms.2015.conf <-
     summarise(acions.dia = n())
 
 # plotando time series
-pl_sms <- ggplot(df_sms.2015.conf, aes(Enviado.em, acions.dia)) + geom_line() + geom_smooth() +
+pl_sms_conf <- ggplot(df_sms.2015.conf, aes(Enviado.em, acions.dia)) + geom_line() + geom_smooth() +
     xlab("dia") + ylab("acionamentos") + ggtitle("SMS com Recebimento Confirmado/Dia") +
     xlim(c(min(df_acion_dia$DIA.ACION),max(df_acion_dia$DIA.ACION))) +
     ylim(c(min(df_sms.2015.tot$acions.dia),max(df_sms.2015.tot$acions.dia)))
@@ -966,10 +959,10 @@ pl_sms_nconf <- ggplot(df_sms.2015.nconf, aes(Enviado.em, acions.dia)) + geom_li
 # ou seja, não podemos prever o movimento de uma variável olhando para a outra
 
 # 1. Correlação sms enviado com confirmação x qtde de pgtos
-my.corr.npg <- df_pg_dia
-my.corr.vlpg <- df_vlpg_dia
-my.corr.npg$DTpgto <- as.character(my.corr.npg$DTpgto)
-my.corr.vlpg$DTpgto <- as.character(my.corr.vlpg$DTpgto)
+#my.corr.npg <- df_pg_dia
+#my.corr.vlpg <- df_vlpg_dia
+#my.corr.npg$DTpgto <- as.character(my.corr.npg$DTpgto)
+#my.corr.vlpg$DTpgto <- as.character(my.corr.vlpg$DTpgto)
 # considerando apenas primeiro pgto
 my.corr.prpg <- df_pg.primpg_dia
 my.corr.prpg$DTpgto <- as.character(my.corr.prpg$DTpgto)
@@ -985,11 +978,11 @@ my.corr.prpg$DTpgto <- as.character(my.corr.prpg$DTpgto)
 my.corr.sms.conf <- df_sms.2015.conf
 my.corr.sms.conf$Enviado.em <- as.character(my.corr.sms.conf$Enviado.em)
 # correlação com nro de pgtos dia
-z2 <- full_join(my.corr.npg,my.corr.sms.conf, by=c("DTpgto" = "Enviado.em"))
+#z2 <- full_join(my.corr.npg,my.corr.sms.conf, by=c("DTpgto" = "Enviado.em"))
 #z2 <- na.omit(z2)
 # x sendo o preditor de y
 # lag negativo significa que x está -n dias atrás de Y, ou seja x leads y, x predict y
-ccf(z2$acions.dia ,z2$pgto.dia, na.action = na.pass)
+#ccf(z2$acions.dia ,z2$pgto.dia, na.action = na.pass)
 
 # There are a lot of models that we could try based on the CCF and lagged scatterplots for
 # these data. For demonstration purposes, we’ll first try a multiple regression in which yt,
@@ -999,22 +992,30 @@ ccf(z2$acions.dia ,z2$pgto.dia, na.action = na.pass)
 # The residuals, however, have an AR(2) structure, as seen in the graph following the
 # regression output. We might try the method described in Lesson 8.1 to adjust for that, 
 # but we’ll take a different approach that we’ll describe after the output display.
-z2 <- na.omit(z2)
-ts1 <- ts(z2$acions.dia)
-ts2 <- ts(z2$pgto.dia)
+#z2 <- na.omit(z2)
+#ts1 <- ts(z2$acions.dia)
+#ts2 <- ts(z2$pgto.dia)
+#ccfvalues <- ccf(ts1, ts2)
+#ccfvalues
+#library("astsa")
+#lag2.plot (ts1, ts2, 15)
+# correlação com nro de primeiros pgtos dia
+z22 <- full_join(my.corr.prpg,my.corr.sms.conf, by=c("DTpgto" = "Enviado.em"))
+#z22 <- na.omit(z22)
+ccf(z22$acions.dia ,z22$pgto.dia, na.action = na.pass)
+
+# teste de lag2.plot
+z22 <- na.omit(z22)
+ts1 <- ts(z22$acions.dia)
+ts2 <- ts(z22$pgto.dia)
 ccfvalues <- ccf(ts1, ts2)
 ccfvalues
 library("astsa")
 lag2.plot (ts1, ts2, 15)
-# correlação com nro de primeiros pgtos dia
-z22 <- full_join(my.corr.prpg,my.corr.sms.conf, by=c("DTpgto" = "Enviado.em"))
-#z22 <- na.omit(z22)
-x <- ccf(z22$acions.dia ,z22$pgto.dia, na.action = na.pass)
-
 # 1. sms enviado com confirmação x valor de pgtos
-z3 <- full_join(my.corr.vlpg,my.corr.sms.conf, by=c("DTpgto" = "Enviado.em"))
+#z3 <- full_join(my.corr.vlpg,my.corr.sms.conf, by=c("DTpgto" = "Enviado.em"))
 #z3 <- na.omit(z3)
-ccf(z3$acions.dia ,z3$pgto.dia, na.action = na.pass)
+#ccf(z3$acions.dia ,z3$pgto.dia, na.action = na.pass)
 # correlação com nro de primeiros pgtos dia
 z33 <- full_join(my.corr.prpg,my.corr.sms.conf, by=c("DTpgto" = "Enviado.em"))
 ccf(z33$acions.dia ,z33$vlpg.dia, na.action = na.pass)
@@ -1023,19 +1024,19 @@ ccf(z33$acions.dia ,z33$vlpg.dia, na.action = na.pass)
 ################################################
 my.corr.sms.nconf <- df_sms.2015.nconf
 my.corr.sms.nconf$Enviado.em <- as.character(my.corr.sms.nconf$Enviado.em)
-z4 <- full_join(my.corr.npg,my.corr.sms.nconf, by=c("DTpgto" = "Enviado.em"))
+#z4 <- full_join(my.corr.npg,my.corr.sms.nconf, by=c("DTpgto" = "Enviado.em"))
 #z4 <- na.omit(z4)
 # correlação com nro de pgtos dia
-ccf(z4$acions.dia ,z4$pgto.dia, na.action = na.pass)
+#ccf(z4$acions.dia ,z4$pgto.dia, na.action = na.pass)
 # com primeiro pgto (qtde)
 z44 <- full_join(my.corr.prpg,my.corr.sms.nconf, by=c("DTpgto" = "Enviado.em"))
 ccf(z44$acions.dia ,z44$pgto.dia, na.action = na.pass)
 
 
 # 2. sms enviado SEM confirmação x valor de pgtos
-z5 <- full_join(my.corr.vlpg,my.corr.sms.nconf, by=c("DTpgto" = "Enviado.em"))
+#z5 <- full_join(my.corr.vlpg,my.corr.sms.nconf, by=c("DTpgto" = "Enviado.em"))
 #z5 <- na.omit(z5)
-ccf(z5$acions.dia ,z5$pgto.dia, na.action = na.pass)
+#ccf(z5$acions.dia ,z5$pgto.dia, na.action = na.pass)
 
 # com primeiro pgto (valor)
 z55 <- full_join(my.corr.prpg,my.corr.sms.nconf, by=c("DTpgto" = "Enviado.em"))
@@ -1044,18 +1045,18 @@ ccf(z55$acions.dia ,z55$vlpg.dia, na.action = na.pass)
 # 3. sms totais enviados
 my.corr.sms.tot <- df_sms.2015.tot
 my.corr.sms.tot$Enviado.em <- as.character(my.corr.sms.tot$Enviado.em)
-z6 <- full_join(my.corr.npg,my.corr.sms.tot, by=c("DTpgto" = "Enviado.em"))
+#z6 <- full_join(my.corr.npg,my.corr.sms.tot, by=c("DTpgto" = "Enviado.em"))
 #z6 <- na.omit(z6)
 # correlação com nro de pgtos dia
-ccf(z6$acions.dia ,z6$pgto.dia, na.action = na.pass)
+#ccf(z6$acions.dia ,z6$pgto.dia, na.action = na.pass)
 # com primeiro pgto (qtde)
 z66 <- full_join(my.corr.prpg,my.corr.sms.tot, by=c("DTpgto" = "Enviado.em"))
 ccf(z66$acions.dia ,z66$pgto.dia, na.action = na.pass)
 
 # 2. sms enviado SEM confirmação x valor de pgtos
-z7 <- full_join(my.corr.vlpg,my.corr.sms.tot, by=c("DTpgto" = "Enviado.em"))
+#z7 <- full_join(my.corr.vlpg,my.corr.sms.tot, by=c("DTpgto" = "Enviado.em"))
 #z7 <- na.omit(z7)
-ccf(z7$acions.dia ,z7$pgto.dia, na.action = na.pass)
+#ccf(z7$acions.dia ,z7$pgto.dia, na.action = na.pass)
 # com primeiro pgto (valor)
 z77 <- full_join(my.corr.prpg,my.corr.sms.nconf, by=c("DTpgto" = "Enviado.em"))
 ccf(z77$acions.dia ,z77$vlpg.dia, na.action = na.pass)
@@ -1291,27 +1292,27 @@ print(pl_tel_atvm, vp = viewport(layout.pos.row = 2, layout.pos.col = 1))
 print(pl_tel_rec, vp = viewport(layout.pos.row = 3, layout.pos.col = 1))
 print(pl_pg, vp = viewport(layout.pos.row = 4, layout.pos.col = 1))
 
-# correlações enre as time series acima
+# correlações entre as time series acima
 
 # nro pgtos x ativas manuais
 my.corr.atvm <- df_tel.2015.atv.m
 my.corr.atvm$Data <- as.character(my.corr.atvm$Data)
-a2 <- full_join(my.corr.npg,my.corr.atvm, by=c("DTpgto" = "Data"))
+#a2 <- full_join(my.corr.npg,my.corr.atvm, by=c("DTpgto" = "Data"))
 #a2 <- na.omit(a2)
 # correlação com nro de pgtos dia
-ccf(a2$acions.dia ,a2$pgto.dia, na.action = na.pass)
-# com primeiro pgto (qtde)
+#ccf(a2$acions.dia ,a2$pgto.dia, na.action = na.pass)
+# com primeiro pgto (qtde) USAR ESTE (PRIM PGTO de TODAS CARTEIRAS x ATIVAS MANUAIS)
 a22 <- full_join(my.corr.prpg,my.corr.atvm, by=c("DTpgto" = "Data"))
 ccf(a22$acions.dia ,a22$pgto.dia, na.action = na.pass)
 
 # nro pgtos x ativas auto
 my.corr.atva <- df_tel.2015.atv.a
 my.corr.atva$Data <- as.character(my.corr.atva$Data)
-a3 <- full_join(my.corr.npg,my.corr.atva, by=c("DTpgto" = "Data"))
-a3 <- na.omit(a3)
+#a3 <- full_join(my.corr.npg,my.corr.atva, by=c("DTpgto" = "Data"))
+#a3 <- na.omit(a3)
 # correlação com nro de pgtos dia
-ccf(a3$acions.dia ,a3$pgto.dia)
-# com primeiro pgto (qtde)
+#ccf(a3$acions.dia ,a3$pgto.dia)
+# com primeiro pgto (qtde) USAR ESTE (PRIM PGTO de TODAS CARTEIRAS x ATIVAS AUTO)
 a33 <- full_join(my.corr.prpg,my.corr.atva, by=c("DTpgto" = "Data"))
 ccf(a33$acions.dia ,a33$pgto.dia, na.action = na.pass)
 # correlação fraca mas negativa (sugestão: não gastar com ativo automatico)
@@ -1319,24 +1320,16 @@ ccf(a33$acions.dia ,a33$pgto.dia, na.action = na.pass)
 # nro pgtos x receptivas
 my.corr.rec <- df_tel.2015.rec
 my.corr.rec$Data <- as.character(my.corr.rec$Data)
-a4 <- full_join(my.corr.npg,my.corr.rec, by=c("DTpgto" = "Data"))
+#a4 <- full_join(my.corr.npg,my.corr.rec, by=c("DTpgto" = "Data"))
 #a4 <- na.omit(a4)
 # correlação com nro de pgtos dia
-ccf(a4$acions.dia ,a4$pgto.dia, na.action = na.pass)
+#ccf(a4$acions.dia ,a4$pgto.dia, na.action = na.pass)
 # com primeiro pgto (qtde)
 a44 <- full_join(my.corr.prpg,my.corr.rec, by=c("DTpgto" = "Data"))
 ccf(a44$acions.dia ,a44$pgto.dia, na.action = na.pass)
 # interessante: correlação mais alta de todas e próximo de lag 0!!!!
 # confirma que existe correlação entre pgto e receptivo
 # conclusão: não perder receptivo:
-
-ts1 <- ts(z2$acions.dia)
-ts2 <- ts(z2$pgto.dia)
-ccfvalues <- ccf(ts1, ts2)
-ccfvalues
-library("astsa")
-lag2.plot (ts1, ts2, 15)
-
 
 # FALTA: obter percentual de abandonos
 
